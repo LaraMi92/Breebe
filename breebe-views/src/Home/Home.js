a757d5948688e78bef44d0101b829dfd5dc4f6d5
LaraMi92 <laraminski@hotmail.fr>
2021-03-23 17:51:07 +0100

    adding tag filtering

--- breebe-views/src/Home/Home.js
+++ breebe-views/src/Home/Home.js
@@ -3,14 +3,18 @@ import {useState, useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
-//== Local imports
+//== Local Components imports
 import './Home.scss';
 import authMw from '../util/auth';
 import Title from '../Title/Title';
 import Input from '../Input/Input';
 import User from '../User/User';
+import Tags from '../Tags/Tags';
+
+// == SVGS
 import breebepen from '../assets/breebepen.svg';
 import breebescribble from '../assets/breebescribble.svg';
+import breebeback from '../assets/breebeback.svg';
 
 const Home = () => {
    const [AccountPage, setAccountPage] = useState(true);
@@ -65,7 +69,7 @@ const Home = () => {
         authMw(history);
         const newBreebe = {
             body: bodyBreebe,
-            tag: "default"
+            tag: null
         };
         const options = {
             url: '/breebe',
@@ -90,11 +94,44 @@ const Home = () => {
    const handleTag = (event) => {
        setTag(event.target.value);
    }
+
+   const filterTags = (id) => {
+        const filteredTag = breebes.filter((breebe) => breebe.breebeId === id);
+        setBreebes(filteredTag);
+   }
+
    const submitEdit = (event) => {
         authMw(history);
         event.preventDefault();
-
+        const edited = {
+            body: editedBreebe,
+            tag: tagBreebe
+        };
+        const options = {
+            url: `/breebe/${breebeId}`,
+            method: 'put',
+            data: edited
+        }
+        const authToken = localStorage.getItem('AuthToken');
+        axios.defaults.headers.common = { Authorization: `${authToken}`};
+        axios(options)
+             .then(() => window.location.reload())
+             .catch((error) => setErrors(error))
        
+   }
+
+   const deleteBreebe = (breebe) => {
+       authMw(history);
+       const authToken = localStorage.getItem('AuthToken');
+       axios.defaults.headers.common = {Authorization: `${authToken}`};
+       let id = breebe.breebeId;
+       axios.delete(`breebe/${id}`)
+            .then(() => {
+                window.location.reload();
+            })
+            .catch((error) => {
+                setErrors(error)
+            })
    }
   return (
   <div>
@@ -112,6 +149,8 @@ const Home = () => {
       />
         <button type="submit" onClick={handleSubmitNew} className="submit-breebe">''</button>
     </form>
+   {breebes.length !== 0 && <Tags breebes={breebes} filterTags={filterTags}/>} 
+
     <div className="breebes">
       {breebes.length !== 0 && breebes.map((breebe, index) => (
                 <div key={index} className="single-breebe">
@@ -122,16 +161,20 @@ const Home = () => {
                      <img src={breebepen} 
                      className="single-breebe--edit"
                      alt="edit breebe"
-                     onClick={() => setEditMode(true)}
+                     onClick={() => {
+                         setEditMode(true);
+                         setBreebeId(breebe.breebeId)
+                        }}
                      />
                      <img src={breebescribble} 
                      className="single-breebe--delete"
                      alt="delete breebe"
+                     onClick={() => deleteBreebe(breebe)}
                      />
                     </div>
                 ) : (
                     <div>
-                    <form>
+                    <form className="single-breebe--form">
                     <input
                     className="single-breebe--input"
                     placeholder={breebe.body}
@@ -149,8 +192,13 @@ const Home = () => {
                     alt="edit breebe"
                     onClick={ editedBreebe === '' ? refuseEdit : submitEdit}
                     />
+                      <img src={breebeback}
+                    className="single-breebe--back" 
+                    onClick={() => setEditMode(false)} 
+                    alt="go back"
+                    />
                     </form>
-                    <button type="button" onClick={() => setEditMode(false)}>retour</button>
+                  
                    </div>
                   
                 )
