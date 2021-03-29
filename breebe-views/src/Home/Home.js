8c93bf30c1b8623cbe0642bf1f3f33db25213371
LaraMi92 <laraminski@hotmail.fr>
2021-03-29 12:02:06 +0200

    login background == signup background

--- breebe-views/src/Home/Home.js
+++ breebe-views/src/Home/Home.js
@@ -39,7 +39,7 @@ const Home = () => {
 
    const history = useHistory();
 
-   const logOut = (event) => {
+   const logOut = () => {
        localStorage.removeItem('AuthToken');
         history.push('/login');
    }
